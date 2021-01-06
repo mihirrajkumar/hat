a4bf792750b6306895ba7a8b8df4c5de67197542
mihirrajkumar <77078237+mihirrajkumar@users.noreply.github.com>
2021-01-06 15:23:47 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+radio.onReceivedValue(function (name, value) {
+	
+})
+basic.forever(function () {
+    basic.showIcon(IconNames.No)
+    basic.showLeds(`
+        # . . . #
+        # # . # #
+        # . # . #
+        # . . . #
+        # . . . #
+        `)
+    basic.showString("music")
+    basic.showNumber(234)
+    music.playMelody("A G A C5 G C5 F A ", 120)
+    music.setVolume(255)
+})
